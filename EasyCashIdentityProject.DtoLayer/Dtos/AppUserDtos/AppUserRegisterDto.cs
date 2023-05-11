d5d92760331fe05a3161113059249120ef9c7a42
gabilgg07 <gabilgg07@gmail.com>
2023-05-11 07:52:54 +0400

    Reletion CustomerAccount and AppUser. Dtos, AppUser Dto

---
+++ EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos
+{
+    internal class AppUserRegisterDto
+    {
+        [Required(ErrorMessage ="Ad yazmaq məcburiyyətindəsiniz!")]
+        [Display(Name ="Ad:")]
+        [MaxLength(50, ErrorMessage ="Ən çox 50 xarakter daxil edə bilərsiz")]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
