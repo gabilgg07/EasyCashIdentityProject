77ca15bcc4c1d63e2c6a42a2428dae008a5dae35
gabilgg07 <gabilgg07@gmail.com>
2023-05-13 06:35:09 +0400

    Added CustomIdentityValidator

---
+++ EasyCashIdentityProject.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+    public class CustomIdentityValidator:IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = $"Şifrə ən az {length} xarakterdən ibarət olmalıdır"
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Zəhmət olmasa, ən az 1 böyük hərf daxil edin"
+            };
+        }
+
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Zəhmət olmasa, ən az 1 kiçik hərf daxil edin"
+            };
+        }
+
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresDigit",
+                Description = "Zəhmət olmasa, ən az 1 rəqəm daxil edin"
+            };
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Zəhmət olmasa, ən az 1 simvol daxil edin"
+            };
+        }
+    }
+}
